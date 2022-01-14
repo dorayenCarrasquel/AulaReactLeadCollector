f6f9d02ea9638cf9d93c50740f2e2f7162ab5ab8
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2022-01-14 10:59:55 -0300

    feat: Make Landing page form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import react, {Component} from "react";
-import Login from "./components/pages/Login";
 import "./App.css"
+import LandigPage from "./components/pages/LandingPage";
 
 
 class App extends Component {
     render(){
         return(
             <div className="App">
-                <Login/>
+                <LandigPage/>
             </div>
         );
     }
--- src/components/pages/LandingPage/index.js
+++ src/components/pages/LandingPage/index.js
@@ -1,1 +1,30 @@
-import react{Component} from "react";
+import react, {Component} from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import Header from "../../Header";
+
+
+export default class LandigPage extends Component{
+    render (){
+        return (
+            <div>
+        <Header title="Comuniquese com nós" />
+        <hr />
+        <Form>
+        <FormGroup>
+            <Label for="nome"> Nome</Label>
+            <Input type="text" id="nome" placeholder="Informe o seu nome: " />
+          </FormGroup>
+          <FormGroup>
+            <Label for="email"> Email</Label>
+            <Input type="text" id="email" placeholder="Informe o seu email: " />
+          </FormGroup>
+          <FormGroup>
+            <Label for="observacoes"> Observações</Label>
+            <Input type="text" id="observavoes" placeholder="Informe se tem alguma Observação " />
+          </FormGroup>
+          <Button color="primary" block> Enviar </Button>
+        </Form>
+      </div>
+        );
+    }
+}
