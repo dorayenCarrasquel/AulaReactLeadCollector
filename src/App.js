18285ed27f3c77b343a831c3f6874441c91f819d
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2022-01-14 14:48:09 -0300

    feat: style forms and backgrounds

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import react, {Component} from "react";
 import "./App.css"
-import LandigPage from "./components/pages/LandingPage";
+import Login from "./components/pages/Login";
+
 
 
 class App extends Component {
     render(){
         return(
             <div className="App">
-                <LandigPage/>
+                <Login/>
             </div>
         );
     }
